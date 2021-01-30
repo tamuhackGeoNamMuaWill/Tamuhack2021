48393919c90f27a841c8585cb75368c3fe3d9eef
WillT23 <39017470+WillT23@users.noreply.github.com>
2021-01-30 15:17:24 -0600

    Add files via upload

---
+++ stock_predictor.py
@@ -0,0 +1,52 @@
+import csv
+import numpy as np
+from sklearn.svm import SVR
+import matplotlib.pyplot as plt
+from pandas_datareader import data as pdr
+from datetime import date
+import yfinance as yf
+yf.pdr_override()
+import pandas as pd
+
+dates = []
+prices = []
+
+def get_data(fileaname):
+  print('test1')
+  with open(fileaname, "r") as csvfile:
+    csvFileReader = csv.reader(csvfile)
+    next(csvFileReader)
+    for row in csvFileReader:
+      dates.append(int(row[0].split("-")[0]))
+      prices.append(float(row[1]))
+  print('test2')
+  return
+
+def predict_price(dates, prices, x):
+  print('test3')
+  dates = np.reshape(dates, (len(dates), 1))
+
+  svr_lin = SVR(kernel = "linear", C = 1e3)
+  svr_poly = SVR(kernel = "poly", C = 1e3, degree = 2)
+  svr_rbf = SVR(kernel = "rbf", C = 1e3, gamma = 0.1)
+  svr_lin.fit(dates, prices)
+  svr_poly.fit(dates, prices)
+  svr_rbf.fit(dates, prices)
+  print('test4')
+
+  plt.scatter(dates, prices, color = "black", label = "Data")
+  plt.plot(dates, svr_rbf.predict(dates), color = "red", label = "RBF model")
+  plt.plot(dates, svr_lin.predict(dates), color = "green", label = "Linear model")
+  plt.plot(dates, svr_poly.predict(dates), color = "blue", label = "Polynomial model")
+  plt.xlabel("Date")
+  plt.ylabel("Price")
+  plt.title("Support Vector Regression")
+  plt.legend()
+  plt.show()
+
+  return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
+
+get_data("AAPL.csv")
+
+predicted_prices = predict_price(dates, prices, 29)
+print(predicted_prices)
