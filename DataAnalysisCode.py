e631c89cbf082c5b1cd28cdb809553faa61ae81d
budgetdoughnut <52521092+budgetdoughnut@users.noreply.github.com>
2021-01-30 20:12:38 -0600

    Update DataAnalysisCode.py

--- DataAnalysisCode.py
+++ DataAnalysisCode.py
@@ -5,17 +5,19 @@ import yfinance as yf
 yf.pdr_override()
 import pandas as pd
 
-ticker_list = []
 files = []
-
+files_SMA = []
+ticker_list = []
 #Dowwnloading Files
-def filesDownload(stocks):
+def filesDownload():
+    global ticker_list
+    global files
+    global files_SMA
     today = date.today()
 
     start_date = "2016-01-01"
-    end_date = date.today()
+    end_date = today
 
-    global files
     def getData(ticker): #downloading data
         print(ticker)
         data=pdr.get_data_yahoo(ticker, start=start_date, end=end_date)
@@ -26,12 +28,18 @@ def filesDownload(stocks):
     def SaveData(df, filename):
         df.to_csv('./data/' + filename + ".csv")
 
+    def SMA(filename):
+        df = pd.read_csv("data/" + filename + ".csv")
+        df['SMA_200'] = df.iloc[:, 5].rolling(window=200).mean()
+        df['SMA_50'] = df.iloc[:, 5].rolling(window=50).mean()
+        dataname = filename + "_with_SMA"
+        files_SMA.append(dataname)
+        SaveData(df, dataname)
+
     for tik in ticker_list:
         getData(tik)
-
-    # for i in range(0,12): #reading csv files
-    #     df1 = pd.read_csv('./data/' + str(files[i]) + '.csv')
-    #     print(df1.head())
+    for i in files:
+        SMA(i)
 
 #Take User input
 def userInput():
@@ -39,29 +47,7 @@ def userInput():
     global ticker_list
     ticker_list = ticker.split()
 
-# Import Data
-
-# Calculate Average for 20 days and 5 days
-def SMA(filename):
-    df = pd.read_csv("data/" + filename + ".csv")
-    df['SMA_200'] = df.iloc[:,5].rolling(window=len(df) - 200).mean()
-    df['SMA_100'] = df.iloc[:, 5].rolling(window=len(df) - 100).mean()
-    df['SMA_50'] = df.iloc[:, 5].rolling(window=len(df) - 50).mean()
-    df['SMA_20'] = df.iloc[:, 5].rolling(window=len(df) - 20).mean()
-    df['SMA_10'] = df.iloc[:, 5].rolling(window=len(df) - 10).mean()
-    df['SMA_5'] = df.iloc[:, 5].rolling(window=len(df) - 5).mean()
-    df['SMA_1'] = df.iloc[:, 5].rolling(window=len(df) - 1).mean()
-    df.to_csv("data/" + filename +"_with_SMA"+".csv")
-
-# Plot Moving Average
-
-# Support and Resistence
-
 
 ######### RUNING CODE ########
-ticker_list = ['AAPL','TSLA','GOOG']
-
-filesDownload(ticker_list)
-for i in files:
-    SMA(i)
-
+userInput()
+filesDownload()
